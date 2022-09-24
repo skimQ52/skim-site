f4c861797bed64fd0bf75b97ee75dad81cae75dd
skylar <skylarmawle@gmail.com>
2022-09-23 21:58:02 -0400

    small changes gotta do work exp

--- src/Home.js
+++ src/Home.js
@@ -7,26 +7,30 @@ const Home = () => {
     return ( 
         <div>
             <div className="Home">
-                <div className="Name">
-                    <HeaderText/>
-                    <div className="intro">
-                        I'm a 
-                        {" "}<TextLoop delay='1000' mask='true' adjustingSpeed='300' springConfig={{ stiffness: 320, damping: 15 }}>
-                            <p>Software Developer</p>
-                            <p>Student of Computing</p>
-                            <p>Leader</p>
-                            <p>Teammate</p>
-                        </TextLoop>
+                <div className="NameContainer">
+                    <div className="Name">
+                        <HeaderText/>
+                        <div className="intro">
+                            I'm a 
+                            {" "}<TextLoop delay='1000' mask='true' adjustingSpeed='300' springConfig={{ stiffness: 320, damping: 15 }}>
+                                <p>Software Developer</p>
+                                <p>Student of Computing</p>
+                                <p>Leader</p>
+                                <p>Teammate</p>
+                            </TextLoop>
+                        </div>
+                        <div className="Picture">
+                            <img src={picture} alt="A picture of myself" className="profilePic"></img>
+                        </div>  
                     </div>
-                    <div className="Picture">
-                        <img src={picture} alt="A picture of myself" className="profilePic"></img>
-                    </div>  
                 </div>
-
-                {/* <div className="Exp">
-                    <h1>Experience</h1>
-                </div> */}
+                <div className="Exp">
+                    <h1 className="titleHeader">Experience</h1>
+                    <h2 className="header">Brock Solutions</h2>
+                    <h2 className="subheader">Software Developer | Current Position</h2>
+                </div>
             </div>
+            
         </div>
     );
 }
