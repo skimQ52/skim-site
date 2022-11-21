56251ff4e09a1a8faff85b00216ad89b54b25145
skylar <skylarmawle@gmail.com>
2022-11-20 21:59:21 -0500

    no more in my life

--- src/Home.js
+++ src/Home.js
@@ -76,11 +76,12 @@ const Home = () => {
                     <div className="chunkAbout">
                         <div className="blockAbout">
                             <p className="textAbout">
-                                <span style={{fontSize: "min(4.5vh, 6vw)", lineHeight: "0.01", color: "white"}}>Hi there!</span> I'm a third year Computer Science student studying at the University of Guelph.
+                                <span style={{fontSize: "min(4.5vh, 6vw)", lineHeight: "0.01", color: "white"}}>Hi there!</span>
+                                 I'm a third year Computer Science student studying at the University of Guelph.
                                 Aside from programming, I love to spend time outside, as well as hone my skills in various 
                                 sports like skateboarding. I have also played basketball, hockey and even wrestled for some time.
                                 Equipped with a big smile and a positive mindset, I am a natural born leader.
-                                Some leadership roles I've taken on in my life include the class of 2020's
+                                Some leadership roles I've taken on include the class of 2020's
                                 Valedictorian at Erin District High School, as well as team captain of the school's rugby team.
                                 I am a valuable asset due to my technical skills,
                                 experience in the field, and my leadership and interpersonal aptitude. 
