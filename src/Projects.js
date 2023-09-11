209f935306328b8cefc758de581bc079b309d60e
skylar <skylarmawle@gmail.com>
2023-09-10 21:13:32 -0400

    FridgeWise added to projects

--- src/Projects.js
+++ src/Projects.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ScrollToTop from './ScrollToTop'
 import Fade from 'react-reveal/Fade';
+import FridgeWise from "./FridgeWise.png";
 import skimageMenu from "./skimageMenu.png";
 import shellQ from "./shellQ.png";
 import boneman from "./boneman.jpg";
@@ -10,6 +11,7 @@ const Projects = () => {
 
     const linkGithub = "Click to see more on GitHub →";
 
+    const listFridgeWise = ['MERN Stack', 'OpenAI API', 'JavaScript', 'HTML/CSS']
     const listSkimage = ['Python', 'NumPy', 'Tkinter', 'Image Processing'];
     const listSVG = ['C', 'Node.js', 'HTML/CSS', 'JQuery'];
     const listShellQ = ['C', 'Threading', 'Unix', 'Operating Systems']
@@ -18,8 +20,16 @@ const Projects = () => {
         <div className="Projects">
             <ScrollToTop />
 
-            {/* SKIMAGE PROCESSOR */}
+            {/* FRIDGEWISE */}
             <Fade left>
+                <div className="itemProjects">
+                    <ProjectItem title="FridgeWise" desc="A Food and Meal tracking service with the ability to AI generate meals!"
+                        list={listFridgeWise} pic={FridgeWise} link="https://github.com/skimQ52/fridgeQ" linkText={linkGithub}></ProjectItem>
+                </div>
+            </Fade>
+
+            {/* SKIMAGE PROCESSOR */}
+            <Fade right>
                 <div className="itemProjects">
                     <ProjectItem title="The 'skImage' Processor" desc="An Image Processing Toolbox capable of performing an array operations under various interpolations."
                         list={listSkimage} pic={skimageMenu} link="https://github.com/skimQ52/skImage-processor" linkText={linkGithub}></ProjectItem>
@@ -27,7 +37,7 @@ const Projects = () => {
             </Fade>
 
             {/* SVG PARSER */}
-            <Fade right>
+            <Fade left>
                 <div className="itemProjects">
                     <ProjectItem title="SVG Parser" desc="A full-stack web application built to parse, display and edit existing Scalable Vector Graphics files."
                         list={listSVG} pic={boneman} link="https://github.com/skimQ52/SVGParser" linkText={linkGithub}></ProjectItem>
@@ -35,7 +45,7 @@ const Projects = () => {
             </Fade>
 
             {/* SHELLQ */}
-            <Fade left>
+            <Fade right>
             <div className="itemProjects">
                 <ProjectItem title="shellQ" desc="A command-line Unix shell which can access and manipulate the operating system's services, mimicing the notorious 'Bash'."
                     list={listShellQ} pic={shellQ} link="https://github.com/skimQ52/shellQ" linkText={linkGithub}></ProjectItem>
@@ -43,7 +53,7 @@ const Projects = () => {
             </Fade>
 
             {/* BONEMAN STAN */}
-            <Fade right>
+            <Fade left>
             <div className="itemProjects">
                 <ProjectItem title="Boneman Stan" desc="A handheld Arduino game console that pairs a 16x2 LED module with a joystick to play the platformer game 'Boneman Stan'."
                     list={listBoneman} pic={boneman}></ProjectItem>
